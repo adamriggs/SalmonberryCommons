75dc7a0af1dc36486933231a7c4b1d214879beaa
Adam Riggs <phlux1111@gmail.com>
2021-04-13 14:53:18 -0400

    mobile menu width and line height

--- wp-content/themes/Salmonberry/dist/woo-bundle.js
+++ wp-content/themes/Salmonberry/dist/woo-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4020ad12b60976edd1c3";
+/******/ 	var hotCurrentHash = "c00d2b66c5bb6ae04cf5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
