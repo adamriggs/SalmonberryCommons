abfbb0b9000767e68dd755700967981e9d3819a7
Adam Riggs <phlux1111@gmail.com>
2021-04-20 11:45:16 -0400

    mobile submit button and spacing

--- wp-content/themes/Salmonberry/dist/woo-bundle.js
+++ wp-content/themes/Salmonberry/dist/woo-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "de39586741363058ca1a";
+/******/ 	var hotCurrentHash = "151ad05782475afd67e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
