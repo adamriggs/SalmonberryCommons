00b68037d9b2f9103183052b5e5cee1f3942f75c
Adam Riggs <phlux1111@gmail.com>
2021-04-21 17:38:09 -0400

    adding acf field for default page template for sidebar image

--- wp-content/themes/Salmonberry/page.php
+++ wp-content/themes/Salmonberry/page.php
@@ -1,10 +1,29 @@
 <?php get_header(); ?>
 
+<?php
+    $sidebar_image = get_field('sidebar_image');
+
+    // echo('<pre>');
+    // print_r($sidebar_image_url);
+    // echo('</pre>');
+?>
+
 <div id="primary" class="content-area">
 	<main id="main" class="site-main single-page" role="main">
         <section class="row">
             <div class="col-3">
-                <img src="<?php echo get_template_directory_uri() . '/images/logo-tall.svg'; ?>">
+                <?php
+                    if(isset($sidebar_image)) {
+                        $sidebar_image_url = $sidebar_image['sizes']['medium'];
+                ?>
+                    <img src="<?php echo $sidebar_image_url; ?>">
+                <?php
+                    } else {
+                ?>
+                    <img src="<?php echo get_template_directory_uri() . '/images/logo-tall.svg'; ?>">
+                <?php 
+                    }
+                ?>
             </div>
 
             <div class="col-6">
