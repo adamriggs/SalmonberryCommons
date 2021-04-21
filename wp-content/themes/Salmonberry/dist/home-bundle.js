0f810fd3ce1292636a7f4f2a8ebca7331747d80a
Adam Riggs <phlux1111@gmail.com>
2021-04-21 12:02:40 -0400

    add to cart button on home page gets cursor pointer

--- wp-content/themes/Salmonberry/dist/home-bundle.js
+++ wp-content/themes/Salmonberry/dist/home-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b793049734b230b78b4a";
+/******/ 	var hotCurrentHash = "4ae0798de6af7797959a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
