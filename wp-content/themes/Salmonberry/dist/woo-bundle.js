e7a2a363ee177cbcada86ed55c159dbbb264bcfe
Adam Riggs <phlux1111@gmail.com>
2021-04-21 10:13:36 -0400

    homepage product tile image sizing

--- wp-content/themes/Salmonberry/dist/woo-bundle.js
+++ wp-content/themes/Salmonberry/dist/woo-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3664699f68e9a880bf75";
+/******/ 	var hotCurrentHash = "94abfa21c6474b3a12cb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
