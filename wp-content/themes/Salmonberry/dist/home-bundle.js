42812efb220642f0d25af6983a1466bb43d3d9c2
Adam Riggs <phlux1111@gmail.com>
2021-04-30 16:47:08 -0400

    how it works section responsiveness

--- wp-content/themes/Salmonberry/dist/home-bundle.js
+++ wp-content/themes/Salmonberry/dist/home-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d234bef6963d577414a";
+/******/ 	var hotCurrentHash = "3423833f7ad737bf0169";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
