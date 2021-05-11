959a01f570c5455f464e44341fcd29f11ce93f72
Adam Riggs <phlux1111@gmail.com>
2021-05-11 12:46:06 -0400

    line height for delivery zone in header

--- wp-content/themes/Salmonberry/dist/home-bundle.js
+++ wp-content/themes/Salmonberry/dist/home-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "de755a30ca69a22d939f";
+/******/ 	var hotCurrentHash = "3bddac47407a93a8d42b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
